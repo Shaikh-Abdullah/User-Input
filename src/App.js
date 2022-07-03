980dd3427ccd5f086819000765b6f359af785e74
Abdullah <shabdullah685@gmail.com>
2022-07-03 14:16:28 +0530

    Initial push

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import AddUser from './components/Users/AddUser';
+import './components/Users/AddUser.module.css'
+
+function App() {
+  return (
+    <div>
+      <AddUser />
+    </div>
+  );
+}
+
+export default App;
