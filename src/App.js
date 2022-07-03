ec1bf0d71e17f5165b4c74c7cc3d1ed340340fe0
Abdullah <shabdullah685@gmail.com>
2022-07-03 17:28:41 +0530

    Using State

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import AddUser from './components/Users/AddUser';
 import './components/Users/AddUser.module.css'
+import UserList from './components/Users/UserList';
 
 function App() {
+  const [usersList, setUsersList] = useState([])
+
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [...prevUsersList, { name: uName, age: uAge }]
+    })
+  }
+
   return (
     <div>
-      <AddUser />
+      <AddUser onAddUser={addUserHandler} />
+      <UserList users={usersList} />
     </div>
   );
 }
